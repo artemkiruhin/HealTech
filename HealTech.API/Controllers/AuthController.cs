6c14a47a66adfde35c3e37c47e8588703c006105
artemkiruhin <temkaunik@gmail.com>
2024-11-25 18:31:15 +0300

    Auth Controller

---
+++ HealTech.API/Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using HealTech.API.RequestModels;
+using HealTech.Application.EntityServices.Base;
+using HealTech.Application.HashServices.Base;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealTech.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        private readonly IHashService _hasher;
+        private readonly ICustomerService _customerService;
+
+        public AuthController(IAuthService authService, IHashService hasher, ICustomerService customerService)
+        {
+            _authService = authService;
+            _hasher = hasher;
+            _customerService = customerService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            try
+            {
+                var token = await _authService.Login(model.Username, _hasher.ComputeHash(model.Password));
+                return Ok(token);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel model)
+        {
+            try
+            {
+                await _customerService.Register(model.FirstName, model.Surname, model.Username, _hasher.ComputeHash(model.Password), model.TaxNumber, model.Email, model.Phone, model.Address);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ HealTech.API/RequestModels/RegisterModel.cs
@@ -0,0 +1,14 @@
+﻿namespace HealTech.API.RequestModels
+{
+    public class RegisterModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string TaxNumber { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+    }
+}
