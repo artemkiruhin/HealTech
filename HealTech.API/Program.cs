8ec089e3397a2e183abd121a208899d4a15f6acc
artemkiruhin <temkaunik@gmail.com>
2024-11-25 18:39:38 +0300

    DI

--- HealTech.API/Program.cs
+++ HealTech.API/Program.cs
@@ -1,6 +1,37 @@
+using HealTech.Application.EntityServices;
+using HealTech.Application.EntityServices.Base;
+using HealTech.Application.HashServices;
+using HealTech.Application.HashServices.Base;
+using HealTech.Application.Jwt;
+using HealTech.Application.Jwt.Base;
+using HealTech.Application.Mappers.AutoMapperService;
+using HealTech.DataAccess.Repositories;
+using HealTech.DataAccess.Repositories.Base;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IProductCategoryRepository, ProductCategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IProductCategoryService, ProductCategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+builder.Services.AddScoped<IHashService, Sha256HashService>();
+builder.Services.AddScoped<IJwtService, JwtService>();
+
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
