382e645bbadfd67cc74ad5ead793944dda01c719
artemkiruhin <temkaunik@gmail.com>
2024-11-25 18:49:01 +0300

    Jwt auth DI and fix

--- HealTech.API/Program.cs
+++ HealTech.API/Program.cs
@@ -7,12 +7,25 @@ using HealTech.Application.Jwt.Base;
 using HealTech.Application.Mappers.AutoMapperService;
 using HealTech.DataAccess.Repositories;
 using HealTech.DataAccess.Repositories.Base;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Настройка параметров JWT
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+builder.Services.AddSingleton<IJwtService>(sp =>
+{
+    var jwtSettings = sp.GetRequiredService<IOptions<JwtSettings>>().Value;
+    return new JwtService(jwtSettings.Key, jwtSettings.Expires, jwtSettings.Audience, jwtSettings.Issuer);
+});
+
+// Регистрация репозиториев и сервисов
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
@@ -28,10 +41,33 @@ builder.Services.AddScoped<IProductCategoryService, ProductCategoryService>();
 builder.Services.AddScoped<IProductService, ProductService>();
 
 builder.Services.AddScoped<IHashService, Sha256HashService>();
-builder.Services.AddScoped<IJwtService, JwtService>();
 
+// Регистрация AutoMapper
 builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
 
+// Настройка аутентификации JWT
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
+    var key = Encoding.UTF8.GetBytes(jwtSettings.Key);
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings.Issuer,
+        ValidAudience = jwtSettings.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ClockSkew = TimeSpan.Zero // Установка нулевой задержки, чтобы токен истекал точно по времени
+    };
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -47,6 +83,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ HealTech.Application/Jwt/JwtSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealTech.Application.Jwt
+{
+    public class JwtSettings
+    {
+        public string Key { get; set; } = string.Empty;
+        public int Expires { get; set; }
+        public string Audience { get; set; } = string.Empty;
+        public string Issuer { get; set; } = string.Empty;
+    }
+
+}
